195ea9bc20b51cb53637a04a0525f679ebc0a9e7
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-08-20 16:35:11 +0530

    Update Calculator.cs

--- MySampleWebApp/Calculator.cs
+++ MySampleWebApp/Calculator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 namespace MySampleWebApp
 {
     public class Calculator
-    {   
-        public string GetPassword()
-        {
-            return "ttt";
-        }
+    {
         public int Add(int a, int b)
         {
             return a + b;
