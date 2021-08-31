81dcbfe82ab3a5af82b83f1f9bc05d3e2272276b
NisargShah1410 <62133719+NisargShah1410@users.noreply.github.com>
2021-08-31 17:49:21 +0530

    Update Calculator.cs

--- MySampleWebApp/Calculator.cs
+++ MySampleWebApp/Calculator.cs
@@ -7,10 +7,21 @@ namespace MySampleWebApp
 {
     public class Calculator
     {
+        public const string password = "SomeString";
         public int Add(int a, int b)
         {
+            if (a==b)
+            {
+
+            }
+            else
+            {
+
+            }
+            string pwd = Constant.DefaultCertificatePassword;
             return a + b;
         }
+
         public int Subtract(int a, int b)
         {
             return b - a;
